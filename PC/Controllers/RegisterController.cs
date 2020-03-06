fee51d3c49ebb4b306535aa8f2141bd8ff619660
Siamion Piatrenka <s.piatrenka@godeltech.com>
2020-03-06 13:36:36 +0300

    registration WIP

--- PC/Controllers/RegisterController.cs
+++ PC/Controllers/RegisterController.cs
@@ -1,14 +1,48 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PC.Auth;
+using PC.Models;
 
 namespace PC.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public RegisterController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         // GET
         [Route("{controller}/")]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("{controller}/")]
+        public async Task<IActionResult> Register(RegisterUserModel user)
+        {
+            User User = new User { Email = user.email, UserName = user.name };
+            var result = await _userManager.CreateAsync(User, user.password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(User, false); 
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return BadRequest();
         }
     }
 }
---
+++ PC/Models/RegisterUserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC.Models
+{
+    public class RegisterUserModel
+    {
+        public string name;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string password;
+
+        [Required]
+        [Display(Name = "Email")]
+        public string email;
+    }
+}
